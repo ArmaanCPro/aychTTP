8d4fb1e21b1488070ce49275117b8e6ae90c0e84
Armaan Chahal <armaan.c2009@gmail.com>
2025-12-10 21:24:12 -0600

    removed modules and added basic tcp socket acceptor

--- src/example_module.cppm
+++
@@ -1,6 +0,0 @@
-export module example_module;
-
-namespace example_module
-{
-    export auto example_module_print() -> void;
-} // namespace test_module
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
-import example_module;
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <string_view>
+#include <iostream>
+
+using namespace boost::asio;
+
+static constexpr auto port = 3000u;
+
+auto my_endpoint()
+{
+
+}
 
 int main()
 {
-    example_module::example_module_print();
+    try
+    {
+        boost::asio::io_context io_context{};
+        ip::tcp::acceptor acceptor(io_context, { ip::tcp::v4(), port });
+
+        for (;;)
+        {
+            ip::tcp::socket socket{io_context};
+            acceptor.accept(socket);
+
+            std::string message = "hi from server";
+            boost::system::error_code ignore_error{};
+            boost::asio::write(socket, boost::asio::buffer(message), ignore_error);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
     return 0;
 }
 
